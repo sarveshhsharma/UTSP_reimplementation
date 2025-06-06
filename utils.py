b5fb1a0f4dd143fe8afb372c06fc1b743f59addb
Sarvesh Sharma <143344743+sarveshhsharma@users.noreply.github.com>
2025-06-07 01:06:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,26 +21,3 @@ def get_heat_map(SctOutput,num_of_nodes):
     return HeatM
 
 
-def edge_overlap(pred,gt_sol):
-    '''
-    gt_sol: the ground truth solution: a list with num_of_nodes nodes
-    pred: pred, an array with (num_of_nodes,top_k)
-    '''
-    gt_edge_set = set()
-    for i in range(pred.shape[0]):
-        gt_edge_set.add((int(gt_sol[i]),int(gt_sol[i+1])))
-        gt_edge_set.add((int(gt_sol[i+1]),int(gt_sol[i])))
-    pred_edge_set = set()
-    for i in range(pred.shape[0]):
-        for j in range(pred.shape[1]):
-            pred_node = pred[i][j]
-            pred_node = int(pred_node)
-            if not i==pred_node:
-                pred_edge_set.add((i,pred_node))
-                pred_edge_set.add((pred_node,i))
-    pred_gt_intsect = pred_edge_set.intersection(gt_edge_set)
-    len_of_pred_gt = len(pred_edge_set)
-    overlap_edge = len(pred_gt_intsect)/2 #here we consider bi-directional, so div 2
-    return overlap_edge,len_of_pred_gt/2
-
-
